5e8049dfdefb454041e7eccab8530599c56f280c
AlexandrSeleznyov <vuavso16@gmail.om>
2021-09-29 08:37:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,34 +5,18 @@ import Section from "./components/Section/Section";
 import Notification from "./components/Notification/Notification";
 
 export default function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const state = { good, neutral, bad };
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
 
   const handleIncrement = (name) => {
-    switch (name) {
-      case "good":
-        setGood((prevState) => prevState + 1);
-        break;
-      case "neutral":
-        setNeutral((prevState) => prevState + 1);
-        break;
-      case "bad":
-        setBad((prevState) => prevState + 1);
-        break;
-      default:
-        break;
-    }
+    setState((prevState) => ({ ...prevState, [name]: prevState[name] + 1 }));
   };
 
   const total = () => {
-    return good + bad + neutral;
+    return Object.values(state).reduce((acc, value) => acc + value, 0);
   };
 
   const positivePercentage = () => {
-    return Math.round((good * 100) / total());
+    return Math.round((state.good * 100) / total());
   };
 
   const options = Object.keys(state);
@@ -46,9 +30,9 @@ export default function App() {
       <Section title="Statistics">
         {isShowStatistics ? (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
             total={total()}
             positivePercentage={positivePercentage()}
           />
